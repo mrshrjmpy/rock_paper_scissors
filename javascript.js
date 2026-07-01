96850fd856cd1bbd003f4ac778628cc1a581629d
aki <mihika.ila@google.com>
2026-07-01 22:38:07 +0530

    complete solution

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+var count = 0;
+var userScore = 0;
+var computerScore = 0;
+
 function getComputerChoice(){
     choice_value = Math.random(0,1);
     if (choice_value < 0.33)
@@ -13,12 +17,11 @@ function getComputerChoice(){
     }
 }
 
-function startGame()
+function playRound(userChoice, computerChoice)
 {
-    var userChoice = prompt("enter choice");
-    var computerChoice = getComputerChoice();
     if(computerChoice == userChoice)
     {
+        console.log("my choice:" + computerChoice);
         console.log("Draw!");
     }
     else if (computerChoice == "rock")
@@ -27,9 +30,11 @@ function startGame()
         if(userChoice == "paper")
         {
             console.log("You win!");
+            userScore ++;
         }
         else {
             console.log("i win!");
+            computerScore ++;
         }
     }
     else if(computerChoice == "paper")
@@ -38,9 +43,11 @@ function startGame()
         if(userChoice == "scissors")
         {
             console.log("You win!");
+            userScore ++;
         }
         else {
             console.log("i win!");
+            computerScore ++; 
         }
     }
     else if (computerChoice == "scissors")
@@ -49,9 +56,37 @@ function startGame()
         if(userChoice == "rock")
         {
             console.log("You win!");
+            userScore ++;
         }
         else {
             console.log("i win!");
+            computerScore ++;
         }
+    }
+
+}
+
+function startGame()
+{
+
+    while(count < 5)
+    {
+        var userChoice = prompt("enter choice");
+        var computerChoice = getComputerChoice();
+        playRound(userChoice,computerChoice);
+        count ++;
+    }
+    console.log("my score:" + computerScore);
+    console.log("your score:" + userScore);
+    if(userScore > computerScore)
+    {
+        console.log("You win the game!");
+    }
+    else if(userScore < computerScore)
+    {
+        console.log("I win the game!");
+    }
+    else {
+        console.log("Draw game!");
     }
 }
